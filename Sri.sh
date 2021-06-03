58f82d56aa70893d9d0380d38597067d4a358ae4
arun2sasi <arun2sasi@gmail.com>
2021-06-03 16:36:14 +0530

    Create Sri.sh

---
+++ Sri.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PROPERTY_FILE=apps.properties
+
+function getProperty {
+   PROP_KEY=$1
+   PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+   echo $PROP_VALUE
+}
+
+echo "# Reading property from $PROPERTY_FILE"
+REPOSITORY_URL=$(getProperty "nexus.repository.url")
